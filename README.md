44a96ec9f5207850b684bed55b5e9bc5a77d0f1e
Francis Ashley <fa.repo.dev@gmail.com>
2019-07-25 03:32:16 +0100

    update readme

--- README.md
+++ README.md
@@ -15,14 +15,34 @@ ReactDOM.render(
   <ShardDocs
     title="Documentation title"
     structure={[
-      { heading: "Essentials" },
-      { title: "Get started", shards: [ <h1>Get started</h1> ] }
+      { type: "heading", heading: "Essentials" },
+      {
+        type: "page",
+        title: "Get started",
+        composition: [
+          <>
+            <h1>Get started</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit.
+              Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis
+              vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales
+              leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+              Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus
+              libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id,
+              est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis.
+              Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc
+              sapien ornare nisl. Phasellus pede arcu, dapibus eu.
+            </p>
+          </>
+        ]
+      }
     ]}
   />,
     document.getElementById('app')
 );
 ```
 
+![Usage example](./example.png "Usage example")
 
 ## Documentation / examples
 Found [here](https://fa-repo.github.io/shard-docs/#/docs).
--- example/src/docs/examples/basic-example.js
+++ example/src/docs/examples/basic-example.js
@@ -6,14 +6,41 @@ import ShardDocs from "shard-docs";
  */
 
 const BasicExample = props => (
+  // <ShardDocs
+  //   title="BasicExample title"
+  //   basePath="/examples/basic-example"
+  //   structure={[
+  //     { type: "heading", heading: "Essentials" },
+  //     { type: "page", title: "Page A", composition: [<h1>Page A</h1>] },
+  //     { type: "page", title: "Page B", composition: [<h1>Page B</h1>] },
+  //     { type: "page", title: "Page C", composition: [<h1>Page C</h1>] }
+  //   ]}
+  // />
   <ShardDocs
-    title="BasicExample title"
+    title="Documentation title"
     basePath="/examples/basic-example"
     structure={[
       { type: "heading", heading: "Essentials" },
-      { type: "page", title: "Page A", composition: [<h1>Page A</h1>] },
-      { type: "page", title: "Page B", composition: [<h1>Page B</h1>] },
-      { type: "page", title: "Page C", composition: [<h1>Page C</h1>] }
+      {
+        type: "page",
+        title: "Get started",
+        composition: [
+          <>
+            <h1>Get started</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit.
+              Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis
+              vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales
+              leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+              Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus
+              libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id,
+              est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis.
+              Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc
+              sapien ornare nisl. Phasellus pede arcu, dapibus eu.
+            </p>
+          </>
+        ]
+      }
     ]}
   />
 );
@@ -22,3 +49,14 @@ BasicExample.propTypes = {};
 BasicExample.defaultProps = {};
 
 export default BasicExample;
+
+// ReactDOM.render(
+//   <ShardDocs
+//     title="Documentation title"
+//     structure={[
+//       { heading: "Essentials" },
+//       { title: "Get started", shards: [ <h1>Get started</h1> ] }
+//     ]}
+//   />,
+//     document.getElementById('app')
+// );
