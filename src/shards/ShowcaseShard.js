726fcfa85fe9da105f89bfaf6441e3e64b335a66
Francis Ashley <fa.repo.dev@gmail.com>
2019-08-16 21:24:15 +0100

    Update ShowcaseShard to support headers.

--- src/shards/ShowcaseShard.js
+++ src/shards/ShowcaseShard.js
@@ -7,21 +7,27 @@ import "./ShowcaseShard.scss";
  * ShowcaseShard
  */
 
-const ShowcaseShard = ({ noPad, ...props }) => {
+const ShowcaseShard = ({ noPad, title, children, ...props }) => {
   return (
-    <div
-      {...props}
-      className={classnames("shard-docs-showcase-shard", props.className)}
-      style={{ padding: noPad && "0px", ...props.style }}
-    />
+    <div {...props} className={classnames("shard-docs-showcase-shard", props.className)}>
+      {title && <header className="shard-docs-showcase-shard-title">{title}</header>}
+      <div
+        style={{ padding: noPad && "0px", ...props.style }}
+        className="shard-docs-showcase-shard-body"
+      >
+        {children}
+      </div>
+    </div>
   );
 };
 
 ShowcaseShard.propTypes = {
+  title: PropTypes.string,
   noPad: PropTypes.bool
 };
 
 ShowcaseShard.defaultProps = {
+  title: "",
   noPad: false
 };
 
