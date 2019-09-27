6d29eec66a4be51971d9d71515f5bee525abea63
fa-repo <fa.repo.dev@gmail.com>
2019-09-27 19:54:24 +0100

    Restructure directory and component relationships

--- src/Sidebar/Header.js
+++ src/Sidebar/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 import MenuIcon from "boxicons/svg/regular/bx-menu.svg";
-import "./SidebarHeader.scss";
+import "./Header.scss";
 
 /**
  * SidebarHeader
