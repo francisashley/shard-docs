9086190f16eacb65d782a1ed95b405d12131b08e
fa-repo <fa.repo.dev@gmail.com>
2019-10-02 03:34:12 +0100

    Add tests for fromSource

--- src/adapters/fromSource.js
+++ src/adapters/fromSource.js
@@ -2,95 +2,130 @@ import slugify from "slugify";
 import kebabCase from "lodash/kebabCase";
 import isArray from "lodash/isArray";
 
-export default function fromSource(source, basePath) {
-  source = source.map(injectType);
-  source = groupAdjacentTopLevelDocuments(source);
-  source = prepareItems(source, basePath);
-  source = prepareBreadcrumbs(source, [{ link: basePath, text: "~" }]);
+export default function fromSource(source, basePath = "/") {
+  source = addTypes(source);
+  source = addPaths(source, basePath);
+  source = addBreadcrumbs(source, [{ link: basePath, text: "~" }]);
+  source = shapeItems(source, basePath);
+  source = combineTopLevelAdjacentItems(source);
   return source;
 }
 
-function injectType(item) {
-  let type = false;
+/**
+ * Loops through each item, child and grandchild detecting and injecting each
+ * items type.
+ * @param  {array} items Source tree.
+ * @return {array}
+ */
+export function addTypes(items) {
+  for (let i in items) {
+    let type = false;
 
-  if (Boolean(item.children)) type = "folder";
-  else if (Boolean(item.document)) type = "document";
-  else if (Boolean(item.externalLink)) type = "external";
+    if (items[i].children) type = "folder";
+    else if (items[i].document) type = "document";
+    else if (items[i].externalLink) type = "external";
 
-  if (type === "folder") {
-    item.children = item.children.map(injectType);
+    if (type === "folder") items[i].children = addTypes(items[i].children);
+
+    items[i].type = type;
   }
 
-  return { ...item, type };
+  return items;
 }
 
-function groupAdjacentTopLevelDocuments(source) {
-  let result = [];
+/**
+ * Combine all top level adjacent items (except folders) into discrete folders.
+ * @param  {array} items Requires types to have been added to array with addTypes().
+ * @return {array}
+ */
+export function combineTopLevelAdjacentItems(items) {
+  const isDiscreteFolder = item => item && item.type === "folder" && !item.title;
 
-  source.map((item, i) => {
-    if (item.type === "document" || item.type === "external") {
-      if (result[result.length - 1] && result[result.length - 1].type === "_folder") {
-        result[result.length - 1].children.push(item);
-      } else {
-        result[result.length] = { title: null, children: [item], type: "_folder" };
-      }
-    } else {
-      result.push(item);
-    }
-    return item;
+  items = items.map(item => {
+    return item.type !== "folder" ? { title: null, children: [item], type: "folder" } : item;
   });
 
-  return result.map(item => (item.type === "_folder" ? { ...item, type: "folder" } : item));
+  return items.reduce((accumulator, item) => {
+    const lastItem = accumulator[accumulator.length - 1];
+
+    if (isDiscreteFolder(item) && isDiscreteFolder(lastItem)) {
+      lastItem.children = [...lastItem.children, ...item.children];
+      accumulator[accumulator.length - 1] = lastItem;
+    } else {
+      accumulator = [...accumulator, item];
+    }
+    return accumulator;
+  }, []);
 }
 
-function prepareItems(items, basePath) {
-  return items
-    .map(item => {
-      const { type, title } = item;
-      const slug = slugify(kebabCase(title), { lower: true });
-      let path = `${basePath}/${slug}`;
-      path = path.replace(/\/+$/, ""); // remove trailing slash
-      path = path.replace(/\/+/g, "/"); // remove duplicate slash
+/**
+ * Loops through each item, child and grandchild detecting and calculating and
+ * and each items path.
+ * @param  {array} items Expects result of fromSource/combineTopLevelAdjacentDocuments().
+ * @return {array}
+ */
+export function addPaths(items, basePath) {
+  const getSlug = title => slugify(kebabCase(title), { lower: true });
 
-      if (type === "external") {
-        return { type, title, link: item.externalLink };
-      }
+  return items.map(item => {
+    // Generate path and remove trailing / duplicate slashes
+    item.path = `${basePath}/${getSlug(item.title)}`.replace(/\/+$/, "").replace(/\/+/g, "/");
 
-      if (type === "folder") {
-        const isEmpty = !item.children.length;
-        const children = prepareItems(item.children, path);
+    if (item.type === "folder") {
+      item.children = addPaths(item.children, item.path);
+    }
 
-        if (title) {
-          return { type, path, title, isEmpty, children };
-        } else {
-          return { type, path, children };
-        }
-      }
-
-      if (type === "document") {
-        const isEmpty = !isArray(item.document) || item.document.length <= 0;
-        const document = item.document;
-
-        return { type, path, title, isEmpty, document };
-      }
-
-      return null;
-    })
-    .filter(Boolean);
+    return item;
+  });
 }
 
-function prepareBreadcrumbs(items, breadcrumbs = []) {
+/**
+ * Recursively loops through source tree adding breadcrumbs to all documents / folders.
+ * @param  {array} items Expects result of fromSource/ shapeItems().
+ * @return {array}
+ */
+export function addBreadcrumbs(items, breadcrumbs = []) {
   return items.map(item => {
     const crumb = { text: item.title, link: item.path };
     if (item.type === "folder") {
       if (item.title) {
-        item.children = prepareBreadcrumbs(item.children, [...breadcrumbs, crumb]);
+        item.children = addBreadcrumbs(item.children, [...breadcrumbs, crumb]);
       } else {
-        item.children = prepareBreadcrumbs(item.children, breadcrumbs);
+        item.children = addBreadcrumbs(item.children, breadcrumbs);
       }
     } else if (item.type === "document") {
       return { ...item, breadcrumbs: [...breadcrumbs, crumb] };
     }
     return item;
   });
+}
+
+/**
+ * Loops through each item, child and grandchild shaping each item to the specs of its given type.
+ * @param  {array} items Expects result of fromSource/combineTopLevelAdjacentDocuments().
+ * @return {array}
+ */
+export function shapeItems(items) {
+  return items
+    .map(item => {
+      const { type, title, path } = item;
+      if (type === "external") {
+        const link = item.externalLink;
+        return { type, title, link };
+      } else if (type === "folder" && title) {
+        const isEmpty = !item.children.length;
+        const children = shapeItems(item.children);
+        return { type, path, title, isEmpty, children };
+      } else if (type === "folder" && !title) {
+        const children = shapeItems(item.children);
+        return { type, path, children };
+      } else if (type === "document") {
+        const isEmpty = !isArray(item.document) || item.document.length <= 0;
+        const document = item.document;
+        const breadcrumbs = item.breadcrumbs;
+        return { type, path, title, isEmpty, breadcrumbs, document };
+      }
+      return null;
+    })
+    .filter(Boolean);
 }
--- src/adapters/fromSource.test.js
+++ src/adapters/fromSource.test.js
@@ -1,7 +1,83 @@
-describe("fromSource()", () => {
-  it("adds all types", () => {});
-  it("removes all unknown types", () => {});
-  it("groups adjacent top level items into discrete folders", () => {});
-  it("wraps single top level items in discrete folders", () => {});
-  it("adds breadcrumbs too all items", () => {});
-});
+import fromSource from "./fromSource";
+
+const source = [
+  { title: "Doc A", document: [] },
+  { title: "Doc B", document: [] },
+  { title: "Doc C", document: [] },
+  {
+    title: "Folder",
+    children: [{ title: "Doc D", document: [] }, { title: "Doc E", document: [] }]
+  },
+  { title: "Github", externalLink: "http://github.com" }
+];
+
+test("fromSource() adapts data", () =>
+  expect(fromSource(source)).toStrictEqual([
+    {
+      title: null,
+      type: "folder",
+      children: [
+        {
+          title: "Doc A",
+          type: "document",
+          path: "/doc-a",
+          breadcrumbs: [{ link: "/", text: "~" }, { text: "Doc A", link: "/doc-a" }],
+          document: [],
+          isEmpty: true
+        },
+        {
+          title: "Doc B",
+          type: "document",
+          path: "/doc-b",
+          breadcrumbs: [{ link: "/", text: "~" }, { text: "Doc B", link: "/doc-b" }],
+          document: [],
+          isEmpty: true
+        },
+        {
+          title: "Doc C",
+          type: "document",
+          path: "/doc-c",
+          breadcrumbs: [{ link: "/", text: "~" }, { text: "Doc C", link: "/doc-c" }],
+          document: [],
+          isEmpty: true
+        }
+      ]
+    },
+    {
+      title: "Folder",
+      type: "folder",
+      path: "/folder",
+      isEmpty: false,
+      children: [
+        {
+          title: "Doc D",
+          type: "document",
+          path: "/folder/doc-d",
+          breadcrumbs: [
+            { link: "/", text: "~" },
+            { text: "Folder", link: "/folder" },
+            { text: "Doc D", link: "/folder/doc-d" }
+          ],
+          document: [],
+          isEmpty: true
+        },
+        {
+          title: "Doc E",
+          type: "document",
+          path: "/folder/doc-e",
+          breadcrumbs: [
+            { link: "/", text: "~" },
+            { text: "Folder", link: "/folder" },
+            { text: "Doc E", link: "/folder/doc-e" }
+          ],
+          document: [],
+          isEmpty: true
+        }
+      ]
+    },
+    {
+      title: null,
+      type: "folder",
+      children: [{ title: "Github", type: "external", link: "http://github.com" }]
+    }
+  ]));
