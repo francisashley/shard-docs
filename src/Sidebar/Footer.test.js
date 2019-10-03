d23ec33bdd0004f2f01853295a58ea23a7d72516
fa-repo <fa.repo.dev@gmail.com>
2019-10-03 02:17:37 +0100

    Write tests for Sidebar/Footer

--- src/Sidebar/Footer.test.js
+++ src/Sidebar/Footer.test.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { mount } from "enzyme";
+import Footer from "./Footer";
 
 describe("<Footer />", () => {
-  test.todo("renders without crashing");
+  it("renders description", () => {
+    const wrapper = mount(<Footer/>)
+    expect(wrapper.find('Footer').exists()).toBe(true)
+  });
 });
