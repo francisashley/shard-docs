6e8d1898a650a3d7933f811e1e87566b9817fb0c
skystash <fa.repo.dev@gmail.com>
2022-07-22 22:33:23 +0100

    Update var name

--- src/prop-types/TreePropType.js
+++ src/prop-types/TreePropType.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import DocumentPropType from "./DocumentPropType";
-import ExternalPropType from "./ExternalLinkPropType";
+import ExternalLinkPropType from "./ExternalLinkPropType";
 import FolderPropType from "./FolderPropType";
 
-export default PropTypes.arrayOf(PropTypes.oneOfType([DocumentPropType, ExternalPropType, FolderPropType]));
+export default PropTypes.arrayOf(PropTypes.oneOfType([DocumentPropType, ExternalLinkPropType, FolderPropType]));
