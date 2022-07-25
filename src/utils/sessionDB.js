b8193a32ca353b83ee8c7ce4928851f2d93659d9
skystash <fa.repo.dev@gmail.com>
2022-07-25 20:28:24 +0100

    Convert sessionDB util to typescript

--- src/utils/sessionDB.js
+++
@@ -1,9 +0,0 @@
-export default {
-  get: (key, defaultValue) => {
-    let value = JSON.parse(sessionStorage.getItem(key));
-    return value !== null ? value : defaultValue;
-  },
-  set: (key, value) => {
-    sessionStorage.setItem(key, JSON.stringify(value));
-  }
-};
---
+++ src/utils/sessionDB.ts
@@ -0,0 +1,9 @@
+export default {
+  get: (key: string, defaultValue: unknown) => {
+    let value = sessionStorage.getItem(key);
+    return value !== null ? JSON.parse(value) : defaultValue;
+  },
+  set: (key: string, value: unknown) => {
+    sessionStorage.setItem(key, JSON.stringify(value));
+  }
+};
