10cf9c705259460abf38e5894b05ba8f20224933
skystash <fa.repo.dev@gmail.com>
2022-07-26 09:50:37 +0100

    Update ShardDocsSidebarLinkNode to properly handle internal / external links

--- src/components/ShardDocsSidebarLinkNode.tsx
+++ src/components/ShardDocsSidebarLinkNode.tsx
@@ -2,28 +2,31 @@ import React from "react";
 import ExternalIcon from "./icons/LinkExternal";
 import { LinkItemPropType } from "../prop-types";
 
-type ExternalLinkNodeProps = {
+type LinkNodeProps = {
   node: linkItem;
 }
 
-const ExternalLinkNode = (props: ExternalLinkNodeProps) => (
-  <li className="shard-docs-menu-external">
-    <a href={props.node.url} target="_blank" style={{ paddingLeft: props.node.depth * 15 + "px" }}>
-      <ExternalIcon />
-      {props.node.name}
-    </a>
-  </li>
-);
+const LinkNode = (props: LinkNodeProps) => {
+  const openInNewTab = props.node.external === true;
+  return (
+    <li className="shard-docs-menu-external">
+      <a href={props.node.url} target={openInNewTab ? "_blank" : "_self"} style={{ paddingLeft: props.node.depth * 15 + "px" }}>
+        <ExternalIcon />
+        {props.node.name}
+      </a>
+    </li>
+  )
+};
 
-ExternalLinkNode.propTypes = {
+LinkNode.propTypes = {
   node: LinkItemPropType
 };
 
-ExternalLinkNode.defaultProps = {
+LinkNode.defaultProps = {
   node: {
     name: "",
     url: ""
   }
 };
 
-export default ExternalLinkNode;
+export default LinkNode;
