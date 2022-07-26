4288ee14db42ad3579b84e4487ccbdf1fdf64f03
skystash <fa.repo.dev@gmail.com>
2022-07-26 19:19:19 +0100

    Configuring react testing library

---
+++ jest-setup.ts
@@ -0,0 +1,43 @@
+import '@testing-library/jest-dom'
+import '@testing-library/user-event'
+
+const originalConsoleError = global.console.error
+
+/**
+ * Ensure propType errors fail the tests.
+ */
+global.beforeEach(() => {
+  global.console.error = (...args) => {
+    const propTypeFailures = [/Failed prop type/, /Warning: Received/]
+
+    if (propTypeFailures.some(p => p.test(args[0]))) {
+      throw new Error(args[0])
+    }
+
+    originalConsoleError(...args)
+  }
+})
+
+// Mock localStorage
+const localStorageMock = (function() {
+  let store = {} as { [key: string]: string }
+
+  return {
+    getItem: function(key: string) {
+      return store[key] || null
+    },
+    setItem: function(key: string, value: any) {
+      store[key] = value.toString()
+    },
+    removeItem: function(key: string) {
+      delete store[key]
+    },
+    clear: function() {
+      store = {}
+    }
+  }
+})()
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock
+})
