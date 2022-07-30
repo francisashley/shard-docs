a09f7df45c7b39d781a9ba6545a8769a8ae25b26
skystash <fa.repo.dev@gmail.com>
2022-07-30 18:31:19 +0100

    Simplify parse script

--- src/utils/dataTools.ts
+++ src/utils/dataTools.ts
@@ -15,141 +15,52 @@ const getInputType = (item: inputItem): "category" | "page" | "link" | null => {
   return null;
 }
 
-function normaliseContent(items: inputData): data {
-  const itemTemplates = {
-    category: { type: 'category', name: '', path: '', items: [], isEmpty: true, isActive: false, depth: 0 },
-    page: { type: 'page', name: '', path: '', breadcrumbs: [], content: null, isEmpty: true, isActive: false, depth: 0 },
-    link: { type: 'link', name: '', url: '', external: false, depth: 0 },
-  }
-
+/**
+ * Parse all user defined content and return a structured array.
+ * @param items 
+ * @param basePath 
+ * @returns 
+ */
+ function parse(items: inputData, basePath: string = "/", depth: number = 0, breadcrumbs: breadcrumb[] = [{ path: '/', name: "~", isActive: false }]): data {
   const output = [];
 
   for (const item of items) {
     const inputType = getInputType(item);
-
-    if (!inputType) continue;
-
-    const outputTemplate = itemTemplates[inputType];
+    const path = removeDuplicateSlashes(`${basePath}/${getSlug(item.name || '')}`);
 
     if (inputType === "category") {
-      const items = normaliseContent((item.content || []) as inputData);
-      output.push({ ...outputTemplate, name: item.name, items } as category);
+      const content = (item.content || []) as inputData;
+      output.push({
+        type: 'category',
+        name: item.name,
+        path,
+        items: parse(content, path, depth + 1, [...breadcrumbs, { name: item.name || '', path, isActive: false }]),
+        isEmpty: !Boolean(content?.length),
+        isActive: false,
+        depth
+      } as category);
     } else if (inputType === "page") {
-      output.push({ ...outputTemplate, name: item.name, content: item.content } as page);
+      output.push({
+        type: 'page',
+        name: item.name,
+        path,
+        breadcrumbs: [...breadcrumbs, { name: item.name, path, isActive: false }],
+        content: item.content,
+        isEmpty: Boolean(!item.content),
+        isActive: false,
+        depth
+      } as page);
     } else if (inputType === "link") {
-      output.push({ ...outputTemplate, name: item.name, url: item.content, external: isExternalLink(item.content as string) } as link);
-    }
-  }
-
-  return output;
-}
-
- function addPaths(items: data, basePath = ''): data {
-    const output = []
-    for (const item of items) {
-      const path = removeDuplicateSlashes(`${basePath}/${getSlug(item.name || '')}`);
-      if (item.type === "category") {
-        output.push({ ...item, path, items: addPaths(item.items || [], path) });
-      } else if (item.type === "page") {
-        output.push({ ...item, path });
-      } else if (item.type === "link") {
-        output.push(item);
-      }
-  }
-  return output;
-}
-
-/**
- * Recursively loops through source tree adding breadcrumbs to all pages / categories.
- * @param  {array} items Expects result of parse / shapeItems().
- * @return {array}
- */
-function addBreadcrumbs(items: data, breadcrumbs: breadcrumb[]): data {
-  const output = [];
-  for (const item of items) {
-    if (item.type === "category") {
       output.push({
-        ...item,
-        items: addBreadcrumbs(item.items || [], [...breadcrumbs, { name: item.name || '', path: item.path, isActive: false }]),
-      })
-    } else if (item.type === "page") {
-      output.push({
-        ...item,
-        breadcrumbs: [...breadcrumbs, { name: item.name, path: item.path, isActive: false }],
-      })
-    } else if (item.type === "link") {
-      output.push(item);
-    }
-  }
-  return output;
-}
-
-/**
- * Loops through each item, child and grandchild shaping each item to the specs of its given type.
- * @param  {array} items Expects result of parse/combineTopLevelAdjacentPages().
- * @return {array}
- */
- function shapeItems(items: data): data {
-  const output = [];
-  for (const item of items) {
-    if (item.type === "category") {
-      const items = shapeItems(item.items || []);
-      const isEmpty = !item.items?.length;
-      output.push({ ...item, isEmpty, items })
-    } else if (item.type === "page") {
-      const isEmpty = !item.content;
-      const depth = item.breadcrumbs?.length || 0;
-      output.push({ ...item, isEmpty, depth })
-    } else if (item.type === "link") {
-      output.push(item)
+        type: 'link',
+        name: item.name,
+        url: item.content,
+        external: isExternalLink(item.content as string),
+        depth
+      } as link);
     }
   }
 
-  return output;
-}
-
-/**
- * Combine all top level adjacent items (except categories) into discrete categories.
- * @param  {array} items Requires types to have been added to array with addTypes().
- * @return {array}
- */
- function combineTopLevelAdjacentItems(items: data): data {
-  const preparedItems = items.map(item => {
-    if (item.type !== "category") {
-      return { name: null, items: [item], type: "category"} as category;
-    }
-    return item
-  });
-
-  const output = [];
-  for (const item of preparedItems) {
-    const lastItem = output[output.length - 1] as item | undefined;
-    if (lastItem?.type === 'category' && !lastItem.name && item.type === 'category' && !item.name) {
-      lastItem.items = [...lastItem.items, ...item.items];
-      output[output.length - 1] = lastItem;
-    } else {
-      output.push(item);
-    }
-  }
-
-  return output;
-}
-
-/**
- * Recursively loops through source tree adding depth level to all items.
- * @param  {array} items Expects result of parse/ shapeItems().
- * @return {array}
- */
-function addDepth(items: data, depth = 0): data {
-  const output = [];
-  for (const item of items) {
-    if (item.type === "category") {
-      const items = addDepth(item.items || [], depth + 1);
-      output.push({ ...item, items, depth })
-    } else {
-      output.push({ ...item, depth })
-    }
-  }
   return output;
 }
 
@@ -181,23 +92,6 @@ function getPrevPage(pages: page[], currentPath: string): page | null {
 function getNextPage(pages: page[], currentPath: string): page | null {
   const activeIndex = pages.findIndex((document: page) => document.path === currentPath);
   return pages[activeIndex + 1] ? pages[activeIndex + 1] : null;
-}
-
-/**
- * Parse all user defined content and return a structured array.
- * @param items 
- * @param basePath 
- * @returns 
- */
-function parse(items: inputData, basePath = "/") {
-  let normalisedItems = normaliseContent(items);
-  normalisedItems = addPaths(normalisedItems, basePath);
-  normalisedItems = addBreadcrumbs(normalisedItems, [{ path: basePath, name: "~", isActive: false }]);
-  normalisedItems = shapeItems(normalisedItems);
-  normalisedItems = combineTopLevelAdjacentItems(normalisedItems);
-  normalisedItems = addDepth(normalisedItems);
-
-  return normalisedItems;
 }
 
 /**
@@ -241,7 +135,6 @@ function setActiveMenuItem(items: data = [], currentPath = "") {
     return item;
   });
 }
-
 
 export default {
   parse,
