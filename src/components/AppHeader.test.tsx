22c9485b221163b9f5b100dc544f77adb85eabcb
skystash <fa.repo.dev@gmail.com>
2022-08-02 00:59:50 +0100

    Fix AppHeader tests and switch to React Testing Library

--- src/components/AppHeader.test.tsx
+++ src/components/AppHeader.test.tsx
@@ -1,58 +1,40 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import AppHeader from './AppHeader'
 
 test('<AppHeader /> renders with default props', () => {
-  const wrapper = mount(
+  render(
     <MemoryRouter>
       <AppHeader />
     </MemoryRouter>
   )
-
-  expect(wrapper.find('.AppHeader').exists()).toBe(true)
+  expect(screen).toBeTruthy()
 })
 
 test('<AppHeader /> renders title', () => {
-  const wrapper = mount(
+  render(
     <MemoryRouter>
       <AppHeader title="Hello world" />
     </MemoryRouter>
   )
-
-  expect(wrapper.find('.AppHeader__title').text()).toBe('Hello world')
+  expect(screen.getByText('Hello world')).toBeTruthy()
 })
 
 test('<AppHeader /> renders header link with custom basePath', () => {
-  const wrapper = mount(
+  render(
     <MemoryRouter>
       <AppHeader title="Hello world" basePath="/docs" />
     </MemoryRouter>
   )
-
-  expect(wrapper.find('.AppHeader__title-link').hostNodes().props().href).toBe('/docs')
-  expect(wrapper.find('.AppHeader').text()).toBe('Hello world')
+  expect(screen.getByRole('link').getAttribute('href')).toBe('/docs')
 })
 
 test('<AppHeader /> renders toggle button', () => {
-  const wrapper = mount(
+  render(
     <MemoryRouter>
       <AppHeader />
     </MemoryRouter>
   )
-
-  expect(wrapper.find('.AppHeader__toggle-btn').exists()).toBe(true)
-})
-
-test('<AppHeader /> calls onToggleMenu when sidebar toggle button clicked', () => {
-  const onToggleMenu = jest.fn()
-  const wrapper = mount(
-    <MemoryRouter>
-      <AppHeader onToggleMenu={onToggleMenu} />
-    </MemoryRouter>
-  )
-
-  wrapper.find('.AppHeader__toggle-btn').simulate('click')
-
-  expect(onToggleMenu.mock.calls[0].length).toBe(1)
+  expect(screen.findByLabelText('Open main menu')).toBeTruthy()
 })
