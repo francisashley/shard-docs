b1843a838407f03e147b26edf171a9c866026dcf
skystash <fa.repo.dev@gmail.com>
2022-08-02 11:26:08 +0100

    Switch AppNav tests to React Testing Library

--- src/components/AppNav.test.tsx
+++ src/components/AppNav.test.tsx
@@ -1,52 +1,41 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import AppNav from './AppNav'
 import dataTools from '../utils/dataTools'
 
 const items = dataTools.parse([{ name: 'Doc A', content: <h1>Doc A</h1> }])
 
-const mountAppNav = (
-  { items, showOnMobile } = {} as { items?: category[]; showOnMobile?: boolean }
-) => {
-  const onNavigate = jest.fn()
-  const wrapper = mount(
+test('<AppNav /> is hidden by default on mobile devices', () => {
+  render(
     <MemoryRouter>
-      <AppNav onNavigate={onNavigate} items={items} showOnMobile={showOnMobile} />
+      <AppNav items={items} device="mobile" />
     </MemoryRouter>
   )
 
-  return { wrapper, onNavigate }
-}
-
-test('<AppNav /> renders with default props', () => {
-  const { wrapper } = mountAppNav()
-
-  expect(wrapper.exists()).toBe(true)
+  expect(screen.queryByRole('navigation')).not.toBeInTheDocument()
 })
 
-test('<AppNav /> renders AppNav', () => {
-  const { wrapper } = mountAppNav({ items } as { items: category[] })
-
-  expect(wrapper.find('.AppNav ul li').exists()).toBe(true)
+test('<AppNav /> can be shown on mobile devices', () => {
+  render(
+    <MemoryRouter>
+      <AppNav items={items} device="mobile" showOnMobile={true} />
+    </MemoryRouter>
+  )
+  expect(screen.queryByRole('navigation')).toBeInTheDocument()
 })
 
-test('<AppNav /> can show AppNav on mobile devices', () => {
-  const { wrapper } = mountAppNav({ items, showOnMobile: true } as { items: category[] })
-
-  expect(wrapper.find('.AppNav--show').exists()).toBe(true)
-})
-
-test('<AppNav /> can show AppNav on mobile devices', () => {
-  const { wrapper } = mountAppNav({ items, showOnMobile: false } as { items: category[] })
-
-  expect(wrapper.find('.AppNav--show').exists()).toBe(false)
-})
-
-test('<AppNav /> calls onNavigate', () => {
-  const { wrapper, onNavigate } = mountAppNav({ items } as { items: category[] })
-
-  wrapper.find('.AppNav ul li a').first().simulate('click')
-
-  expect(onNavigate.mock.calls.length).toBe(1)
+test('<AppNav /> is always visible on desktop', () => {
+  const { rerender } = render(
+    <MemoryRouter>
+      <AppNav items={items} device="desktop" />
+    </MemoryRouter>
+  )
+  expect(screen.queryByRole('navigation')).toBeInTheDocument()
+  rerender(
+    <MemoryRouter>
+      <AppNav items={items} device="desktop" />
+    </MemoryRouter>
+  )
+  expect(screen.queryByRole('navigation')).toBeInTheDocument()
 })
