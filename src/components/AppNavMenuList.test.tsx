a5bbcd21bf7d300f0443bf0ddb18f89cac63f77c
skystash <fa.repo.dev@gmail.com>
2022-08-02 13:03:16 +0100

    Add tests for AppNavMenuList

---
+++ src/components/AppNavMenuList.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import AppNavMenuList from './AppNavMenuList'
+import { MemoryRouter } from 'react-router-dom'
+import dataTools from '../utils/dataTools'
+
+const items = dataTools.parse([
+  {
+    name: 'Examples',
+    content: [{ name: 'Example A', content: <h1>Doc A</h1> }],
+  },
+])
+
+test('Renders menu', () => {
+  render(
+    <MemoryRouter>
+      <AppNavMenuList items={items} />
+    </MemoryRouter>
+  )
+
+  expect(screen.getByRole('list')).toBeInTheDocument()
+})
+
+test('Hides menu', () => {
+  render(
+    <MemoryRouter>
+      <AppNavMenuList items={items} hidden />
+    </MemoryRouter>
+  )
+
+  expect(screen.queryByRole('list')).not.toBeInTheDocument()
+})
