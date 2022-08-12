46a8aa269002c729ad3f53881c7a60864e8d3d93
skystash <fa.repo.dev@gmail.com>
2022-08-12 21:09:05 +0100

    Update docs to run on vite

---
+++ docs/src/main.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MDXProvider } from '@mdx-js/react'
+import ShardDocs, { CodeBlockRenderer } from '../../dist/index'
+
+import '../../dist/index.css'
+
+// Content
+import EssentialsGetStartedContent from '../content/1-essentials-get-started.mdx'
+import EssentialsSchemaReferenceContent from '../content/1-source-schema.mdx'
+import EssentialsApiReferenceContent from '../content/1-reference-api.mdx'
+import ExamplesHelloWorldContent from '../content/2-examples-hello-world.mdx'
+import ShardsCodeSampleShardContent from '../content/3-shards-code-sample.mdx'
+import ShardsSectionShardContent from '../content/3-shards-section.mdx'
+
+const components = {
+  pre: (props: any) => {
+    if (props?.children?.props?.mdxType === 'code') {
+      return props.children
+    } else {
+      return <pre {...props} />
+    }
+  },
+  code: CodeBlockRenderer,
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <MDXProvider components={components}>
+      <ShardDocs
+        title="Shard docs"
+        data={[
+          { name: 'Get started', content: <EssentialsGetStartedContent /> },
+          { name: 'Content', content: <EssentialsSchemaReferenceContent /> },
+          { name: 'API', content: <EssentialsApiReferenceContent /> },
+          {
+            name: 'Examples',
+            content: [{ name: 'Hello world', content: <ExamplesHelloWorldContent /> }],
+          },
+          {
+            name: 'Shards',
+            content: [
+              { name: '<CodeSample />', content: <ShardsCodeSampleShardContent /> },
+              { name: '<Section />', content: <ShardsSectionShardContent /> },
+            ],
+          },
+          { name: 'Github', content: 'http://github.com/fa-repo/shard-docs' },
+        ]}
+      />
+    </MDXProvider>
+  </React.StrictMode>
+)
---
+++ docs/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
+import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    viteCommonjs(),
+    mdx({
+      // See https://mdxjs.com/advanced/plugins
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypePrism],
+    }),
+    react(),
+  ],
+
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        // Solves:
+        // https://github.com/vitejs/vite/issues/5308
+        // add the name of your package
+        esbuildCommonjs(['react-frame-component']),
+      ],
+    },
+  },
+})
