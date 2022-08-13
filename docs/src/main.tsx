abba6c93c557a99590bd8593ebe98107591c3d80
skystash <fa.repo.dev@gmail.com>
2022-08-13 13:47:05 +0100

    Remove unnuecessary mdx provider

--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MDXProvider } from '@mdx-js/react'
-import ShardDocs, { CodeBlockRenderer } from '../../dist/index'
+import ShardDocs from '../../dist/index'
 
 import '../../dist/index.css'
 
@@ -12,36 +11,23 @@ import EssentialsApiReferenceContent from '../content/1-reference-api.mdx'
 import ShardsCodeSampleShardContent from '../content/3-shards-code-sample.mdx'
 import ShardsSectionShardContent from '../content/3-shards-section.mdx'
 
-const components = {
-  pre: (props: any) => {
-    if (props?.children?.props?.mdxType === 'code') {
-      return props.children
-    } else {
-      return <pre {...props} />
-    }
-  },
-  code: CodeBlockRenderer,
-}
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MDXProvider components={components}>
-      <ShardDocs
-        title="Shard docs"
-        data={[
-          { name: 'Get started', content: <EssentialsGetStartedContent /> },
-          { name: 'Content', content: <EssentialsSchemaReferenceContent /> },
-          { name: 'API', content: <EssentialsApiReferenceContent /> },
-          {
-            name: 'Shards',
-            content: [
-              { name: '<CodeSample />', content: <ShardsCodeSampleShardContent /> },
-              { name: '<Section />', content: <ShardsSectionShardContent /> },
-            ],
-          },
-          { name: 'Github', content: 'http://github.com/fa-repo/shard-docs' },
-        ]}
-      />
-    </MDXProvider>
+    <ShardDocs
+      title="Shard docs"
+      data={[
+        { name: 'Get started', content: <EssentialsGetStartedContent /> },
+        { name: 'Content', content: <EssentialsSchemaReferenceContent /> },
+        { name: 'API', content: <EssentialsApiReferenceContent /> },
+        {
+          name: 'Shards',
+          content: [
+            { name: '<CodeSample />', content: <ShardsCodeSampleShardContent /> },
+            { name: '<Section />', content: <ShardsSectionShardContent /> },
+          ],
+        },
+        { name: 'Github', content: 'http://github.com/fa-repo/shard-docs' },
+      ]}
+    />
   </React.StrictMode>
 )
