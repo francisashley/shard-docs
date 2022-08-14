a7d20a78e2274a344c83eb67ee6d2f34c4f4e9c0
skystash <fa.repo.dev@gmail.com>
2022-08-14 20:24:50 +0100

    Switch back to using docs.config.tsx again

--- docs/scripts/run/index.tsx
+++ docs/scripts/run/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ShardDocs from '../../../dist/index'
-import config from '../../entry'
+import config from '../../docs.config'
 import '../../../dist/index.css'
 
 type dataItem = {
