de5516ca766b188442f753f536e621b22f286b09
Francis Ashley <fa.repo.dev@gmail.com>
2022-08-28 21:04:06 +0100

    Display vite output when running cli commands

--- src/cli/shard-docs.ts
+++ src/cli/shard-docs.ts
@@ -3,7 +3,7 @@
 import chalk from 'chalk'
 import fs from 'fs-extra'
 import arg from 'arg'
-import { exec } from 'node:child_process'
+import { spawn } from 'node:child_process'
 import path from 'path'
 import url from 'url'
 import Table from 'cli-table'
@@ -49,17 +49,23 @@ export function cli(args: string[]) {
         [chalk.blue('Entry File'), paths.entryFile],
       ],
     })
-
     console.log(table.toString())
+  }
+
+  const run = (input: string) => {
+    const inputParts = input.split(' ')
+    const build = spawn(inputParts[0], inputParts.slice(1), { stdio: 'inherit' })
+    build.stdout?.on('data', (data) => console.log(data.toString()))
+    build.stderr?.on('data', (data) => console.error(data.toString()))
+    build.on('exit', (code) => console.log('child process exited with code ' + code))
   }
 
   fs.copy(paths.templateFolder, paths.docsFolder, (err) => {
     if (err) return console.error(err)
-
     if (options.command === 'build') {
-      exec(`vite build ${paths.docsFolder} --outDir ../.build --emptyOutDir`)
+      run(`vite build ${paths.docsFolder} --outDir ../.build --emptyOutDir`)
     } else if (options.command === 'dev') {
-      exec(`vite ${paths.docsFolder}`)
+      run(`vite ${paths.docsFolder}`)
     }
   })
 }
