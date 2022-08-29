21ca8e23228078735882a29e05f3c0dc20c2a58f
Francis Ashley <fa.repo.dev@gmail.com>
2022-08-29 21:08:31 +0100

    Update the docs

--- docs/docs.config.tsx
+++ docs/docs.config.tsx
@@ -1,20 +1,20 @@
-import EssentialsGetStartedContent from './content/1-essentials-get-started.mdx'
-import EssentialsSchemaReferenceContent from './content/1-source-schema.mdx'
-import EssentialsApiReferenceContent from './content/1-reference-api.mdx'
-import ShardsCodeSampleShardContent from './content/3-shards-code-sample.mdx'
-import ShardsSectionShardContent from './content/3-shards-section.mdx'
+import GetStartedContent from './content/get-started.mdx'
+import ConfigContent from './content/config.mdx'
+import DataContent from './content/data.mdx'
+import CodeSampleShardContent from './content/shards/code-sample-shard.mdx'
+import SectionShardContent from './content/shards/section-shard.mdx'
 
 export default {
   title: 'Shard docs',
   data: [
-    { name: 'Get started', content: <EssentialsGetStartedContent /> },
-    { name: 'Content', content: <EssentialsSchemaReferenceContent /> },
-    { name: 'API', content: <EssentialsApiReferenceContent /> },
+    { name: 'Get started', content: <GetStartedContent /> },
+    { name: 'Config', content: <ConfigContent /> },
+    { name: 'Data', content: <DataContent /> },
     {
       name: 'Shards',
       content: [
-        { name: '<CodeSample />', content: <ShardsCodeSampleShardContent /> },
-        { name: '<Section />', content: <ShardsSectionShardContent /> },
+        { name: '<CodeSample />', content: <CodeSampleShardContent /> },
+        { name: '<Section />', content: <SectionShardContent /> },
       ],
     },
     { name: 'Github', content: 'http://github.com/fa-repo/shard-docs' },
